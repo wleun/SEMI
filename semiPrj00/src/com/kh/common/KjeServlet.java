fa857ee6301e0c7899aab4e6bfcbd256bedc8ca7
MSDN-SPECIAL\Administrator <kkje1872@naver.com>
2022-08-03 17:11:23 +0900

    인사하는 서블릿~

---
+++ semiPrj00/src/com/kh/common/KjeServlet.java
@@ -0,0 +1,17 @@
+package com.kh.common;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/kje")
+public class KjeServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("안녕요");
+	}
+}
