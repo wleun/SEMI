80479f3c4a15865953bdb43f2b6aef71acf3c6d0
DESKTOP-CH3HBB6\user <dmswl9782@gmail.com>
2022-08-16 21:52:33 +0900

    오픈폼 컨트롤러 작업중

--- semiPrj00/src/com/kh/project/controller/PrjOpenController.java
+++ semiPrj00/src/com/kh/project/controller/PrjOpenController.java
@@ -22,6 +22,53 @@ public class PrjOpenController extends HttpServlet{
 	//오픈 폼 데이터 처리
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		
+		//데이터 꺼내기
+		
+		//1page 프로젝트 기본정보
+		String category = req.getParameter("prjCategory"); //카테고리
+		String title = req.getParameter("prjTitle"); //프로젝트 제목
+		String startDate = req.getParameter("startDate");//시작일(날짜)////
+		String endDate = req.getParameter("endDate");//마감일(날짜)////
+		String goal = req.getParameter("prjAmount"); //목표금액
+		//대표이미지
+		//상세이미지
+		String text = req.getParameter("prjText"); //상세설명
+		String etc = "";
+		if(req.getParameter("etc") != null) {
+			etc = req.getParameter("etc"); //예상 어려움 ////
+		} 
+		//확인용
+		System.out.println(category);
+		System.out.println(title);
+		System.out.println(startDate);
+		System.out.println(endDate);
+		System.out.println(goal);
+		System.out.println(etc);
+		
+		
+		
+		
+		
+		
+//		
+//		int shippingDate = Integer.parseInt(req.getParameter("shippingDate")); //결제 마감일로부터 n일 
+//		String shippingDateCalc = endDate + 7 + shippingDate; //배송일(날짜) 마감일 + 결제 7일 + 지정한 날짜 n일 ////
+//		
+//		
+//		//2page 창작자 정보
+//		if(req.getParameter("makerInfo") != null) {
+//		String makerInfo = req.getParameter("makerInfo");
+//		} else {
+//			String makerInfo = "";
+//		}
+//		
+//		
+//		
+		
+		
+		
 		
 	}
 }
