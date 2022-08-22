22cff43f41769488d5d17215f70fa8be5fc3e1ff
MSDN-SPECIAL\Administrator <kkje1872@naver.com>
2022-08-22 20:38:45 +0900

    임시커밋

--- semiPrj00/src/com/kh/memberNotice/Controller/NoticeListController.java
+++ semiPrj00/src/com/kh/memberNotice/Controller/NoticeListController.java
@@ -1,6 +1,7 @@
 package com.kh.memberNotice.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,10 +9,64 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.admin.notice.vo.AdminNoticeVo;
+import com.kh.common.vo.PageVo;
+import com.kh.memberNotice.Service.NoticeService;
+
 @WebServlet(urlPatterns = "/member/noticeList")
 public class NoticeListController extends HttpServlet{
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//페이징 처리
+//		int listCount;
+//		int currentPage;
+//		int pageLimit;
+//		int boardLimit;
+//		
+//		int maxPage;
+//		int startPage;
+//		int endPage;
+//		
+//		//listCount값 구하기
+//		listCount = new NoticeService().getCount();
+//		
+//		//currentPage값 구하기
+//		currentPage = Integer.parseInt(req.getParameter("p"));
+//		
+//		//pageLimit값 구하기
+//		pageLimit = 5;
+//		
+//		//boardLimit값 구하기
+//		boardLimit = 5;
+//		
+//		maxPage = (int)Math.ceil((double)(listCount/boardLimit));
+//		
+//		startPage = (currentPage-1)/pageLimit * pageLimit + 1;
+//		
+//		endPage = startPage + pageLimit -1;
+//		if(endPage>maxPage) {
+//			endPage = maxPage;
+//		}
+//		
+//		//vo에 페이지 관련 변수담기
+//		PageVo pageVo = new PageVo();
+//		pageVo.setBoardLimit(boardLimit);
+//		pageVo.setCurrentPage(currentPage);
+//		pageVo.setEndPage(endPage);
+//		pageVo.setListCount(listCount);
+//		pageVo.setMaxPage(maxPage);
+//		pageVo.setPageLimit(pageLimit);
+//		pageVo.setStartPage(startPage);
+//		
+//		//게시글 관련 데이터 준비
+//		List<AdminNoticeVo> boardVoList = new NoticeService().selectList(pageVo);
+//		
+//		//준비한 데이터 담기
+//		req.setAttribute("pv", pageVo);
+//		req.setAttribute("list", boardVoList);
+		//화면 보여주기
 		req.getRequestDispatcher("/WEB-INF/views/noticeEvent/notice.jsp").forward(req, resp);
 	}
 }
---
+++ semiPrj00/src/com/kh/memberNotice/Service/NoticeService.java
@@ -0,0 +1,51 @@
+package com.kh.memberNotice.Service;
+
+import java.sql.Connection;
+
+import com.kh.admin.notice.vo.AdminNoticeVo;
+import com.kh.common.vo.PageVo;
+import com.kh.memberNotice.dao.NoticeDao;
+import java.util.*;
+
+import static com.kh.common.JDBCTemplate.*;
+
+public class NoticeService {
+	private final NoticeDao dao = new NoticeDao();
+	
+	//listCount값 구하기 현재 총 게시글 갯수
+	public int getCount() {
+	
+		
+		int result = 0;
+		Connection conn = null;
+		
+		try {
+			conn = getConnection();
+			result = dao.getCount(conn);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			close(conn);
+		}
+		return result;
+	}
+	/*
+	 * 게시글 리스트 조회
+	 */
+
+	public List<AdminNoticeVo> selectList(PageVo pageVo) {
+		
+		Connection conn = null;
+		List<AdminNoticeVo> voList = null;
+		
+		conn= getConnection();
+		//dao호출
+		voList = dao.selectList(conn,pageVo);
+		
+		close(conn);
+		
+		return voList;
+	}
+	
+
+}
