f2b43c59285a1ef866909848be1cb8f00ab7cfbf
LAPTOP-9PM7B9FO\yewon <a>
2022-08-24 23:01:16 +0900

    마이페이지 내가 올린 프로젝트 팔로우

--- semiPrj00/src/com/kh/member/controller/MemberMyfollowController.java
+++ semiPrj00/src/com/kh/member/controller/MemberMyfollowController.java
@@ -1,6 +1,7 @@
 package com.kh.member.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.member.service.MemberMyfollowService;
+import com.kh.member.vo.MemberMyfollowVo;
+import com.kh.member.vo.MemberVo;
+
 @WebServlet(urlPatterns = "/member/myfollow")
 public class MemberMyfollowController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String no = ((MemberVo)req.getSession().getAttribute("loginMember")).getNo();
 		
+		List<MemberMyfollowVo> fVoList = new MemberMyfollowService().selectFollow(no);
+		
+		req.setAttribute("fVoList", fVoList);
 		req.getRequestDispatcher("/WEB-INF/views/member/memberFollowPage.jsp").forward(req, resp);
 		
 	}
